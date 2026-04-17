bfe50a194d9c7c652e366be179845799cfcd33f7
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-16 21:48:24 -0300

    aula construtores

---
+++ com/fiapride/main/SistemaPrincipal.java
@@ -0,0 +1,12 @@
+package br.com.fiapride.main;
+
+import br.com.fiapride.model.Passageiro;
+import br.com.fiapride.model.Veiculo;
+
+public class SistemaPrincipal {
+
+	public static void main(String[] args) {
+
+		Veiculo meuCarro = new Veiculo("ABC-1234", "Toyota Corolla");
+	}
+}
